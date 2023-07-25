c387dc27b875aacec54e9da9f3571a937267b00d
Mattwhited <matthewwhited@hotmail.com>
2023-07-24 20:32:58 -0500

    Further define computer and player selection
    I gave each selection more information

--- java.js
+++ java.js
@@ -2,11 +2,9 @@
 let playerSelection;
 let computerSelection;
 
-
-
-    // if the answer is equal to 0 make it rock
-    // if the answer is equal to 1 make it paper
-    // if the answer is equal to 2 make it scissors 
-
 playerSelection = prompt("Choose Rock, Paper, or Scissors:");
+computerSelection = Math.floor(Math.random()* 3 + 1)
 
+// if the answer is equal to 0 make it rock
+// if the answer is equal to 1 make it paper
+// if the answer is equal to 2 make it scissors 
