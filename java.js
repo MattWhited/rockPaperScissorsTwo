0dec1a33e6ea0fcb58e0a7b99ef4838513783fae
Mattwhited <matthewwhited@hotmail.com>
2023-07-28 11:16:35 -0500

    Fiddle around with my code.
    honestly I have just been trying to get my
    "getComputerChoice" function to spit out the
    correct answer.

--- java.js
+++ java.js
@@ -5,6 +5,17 @@ let computerSelection;
 playerSelection = prompt("Choose Rock, Paper, or Scissors:");
 computerSelection = Math.floor(Math.random()* 3 + 1)
 
+function getComputerChoice (computerSelection) {{
+   } if (computerSelection = 1) {
+        return ("Rock");
+     } else if (computerSelection = 2) {
+        return ("Paper");
+     } else {
+        return ("Scissors");
+     }
+}
+
+
 // if the answer is equal to 0 make it rock
 // if the answer is equal to 1 make it paper
 // if the answer is equal to 2 make it scissors 
