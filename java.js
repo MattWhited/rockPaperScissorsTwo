0e99e8e8a0b3f791f5445620736712c74a49389d
Mattwhited <matthewwhited@hotmail.com>
2023-10-24 15:53:29 -0500

    add a check list for what I plan on doing.
    I wanted a place keep a check list of things
    that I need to be doing so I don't loose sight
    of the goal.

--- java.js
+++ java.js
@@ -89,3 +89,32 @@ function bestOfFive(){
 // either but I think it is more important to 
 // move on and learn more instead of being a 
 // perfectionist. 
+
+
+
+// Check List
+// I am putting my assignment/Check List here
+// so I can reference what I am supposed to be
+// doing with out always going back to it. 
+
+
+// In our UI, the player should be able to play 
+//the game by clicking on buttons rather than 
+//typing their answer in a prompt.
+
+//1) For now, remove the logic that plays exactly
+// five rounds.
+//2) Create three buttons, one for each 
+// selection. Add an event listener to the buttons 
+// that call your playRound function with the 
+// correct playerSelection every time a button 
+// is clicked. (you can keep the console.logs 
+// for this step)
+//3) Add a div for displaying results and change
+// all of your console.logs into DOM methods.
+//4) Display the running score, and announce a 
+// winner of the game once one player reaches 5 points.
+//5) You will likely have to refactor (rework/rewrite)
+// your original code to make it work for this. 
+// That’s OK! Reworking old code is an important
+// part of a programmer’s life.
